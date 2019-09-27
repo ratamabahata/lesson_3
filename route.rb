69ed9308b6100e106c44c88457b03104c50041ff
cd c:\rubytut\lesson7 <shirma-michail@mail.ru>
2019-09-27 16:38:05 +0300

    2 comit

--- route.rb
+++ route.rb
@@ -2,21 +2,20 @@ class Route
   attr_reader :stations
  #создание первых и последних станций 
   def initialize(first, last)
-  	@path = [first, last]
+  @path = [first, last]
   end
 #добавление новых станций
   def add_station(station)
-    @stations.insert(-2, station)
+  @stations.insert(-2, station)
   end
 #удаление станций
   def delete_station(station)
-    @stations.delete(station)
+  @stations.delete(station)
   end
 #ввывод списка станций
-def station_list
-    @stations.each do |sp|
-      puts sp
-    end
-
-end
+  def station_list
+  @stations.each do |spisok|
+  puts " > #{spisok}"
+  end
+end 
 end
--- train.rb
+++ train.rb
@@ -4,37 +4,37 @@ class Train
   attr_reader :wagon_count, :current_station, :number, :type_wagon
 
 #Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов
-def initialize(number, type, wagon_count)
-    @number = number
-    @type = type
-    @wagon_count = wagon_count
-    @speed = 0
+  def initialize(number, type, wagon_count)
+  @number = number
+  @type = type
+  @wagon_count = wagon_count
+  @speed = 0
   end
 #состояние покоя
-def stop
-    self.speed = 0
+  def stop
+  self.speed = 0
   end
 #функция добавления вагонов
- def add_wagon
-    @wagon_count += 1 if speed.zero?
+  def add_wagon
+  @wagon_count += 1 if speed.zero?
   end
 #удаление вагонов
   def remove_wagon
-    @wagon_count -= 1 if speed.zero? && wagon_count > 0
+  @wagon_count -= 1 if speed.zero? && wagon_count > 0
   end
 #определение маршрута
-def define_route(route)
-    @route = route
-    station = route.path.first
-    station.take_train(self)
-    @current_station = station
+  def define_route(route)
+  @route = route
+  station = route.path.first
+  station.take_train(self)
+  @current_station = station
   end
 #прибыватие на станцию
   def arrive_at_station(station)
     if [prev_station, next_station].include?(station)
-      current_station.delete_train(self)
-      station.take_train(self)
-      @current_station = station
+    current_station.delete_train(self)
+    station.take_train(self)
+    @current_station = station
     end
   end
 #перемещение вперед на станцию
